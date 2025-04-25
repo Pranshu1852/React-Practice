2a77ae746384089ffd3a3f75651e81d730f1f1e1
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-25 18:29:31 +0530

    Add(tic-tac-toe): Create winning logic of the game

--- React-TIC-TAC-TOE/src/components/GameBoard.jsx
+++ React-TIC-TAC-TOE/src/components/GameBoard.jsx
@@ -1,19 +1,11 @@
 import { useState } from "react"
 
-let initialGameBoard=[[null, null, null] ,[ null, null, null], [null ,null, null]]
 
-export default function GameBoard({onSelectSquare, turns}) {
-    let gameBoard=initialGameBoard;
 
-    for(const turn of turns){
-        const {square,player}=turn;
-        const {row,col}=square;
-
-        gameBoard[row][col]=player;
-    }
+export default function GameBoard({onSelectSquare, board}) {
 
     return <ol id="game-board">
-        {gameBoard.map((row, rowIndex)=>{
+        {board.map((row, rowIndex)=>{
             return <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol,colIndex)=>{
