04c7024af03e89af008afcadf9af4c15d3a54dfd
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-28 15:21:58 +0530

    Add(investment-calculator): Create result section in investment calculator

--- REACT-INVESTMENT-CALCULATOR/src/App.jsx
+++ REACT-INVESTMENT-CALCULATOR/src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Userinput from './components/UserInput'
+import Results from './components/Results'
 
 function App() {
+  const [userInput, setuserInput]=useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  const inputIsValid=userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+      setuserInput((prevUserInput)=>{
+          return {
+              ...prevUserInput,
+              [inputIdentifier]: +newValue,
+          }
+      })
+  }
+
   return (
     <>
       <Navbar/>
-      <Userinput/>
+      <Userinput userInput={userInput} onChange={handleChange}/>
+      {inputIsValid ? <Results input={userInput}/> : <p className='center'>Please enter a duration greater than zero.</p>}
     </>
   )
 }
