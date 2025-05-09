8e24f482903b22dcb6368b2dc47c4bfbb93776e6
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-05-09 15:16:31 +0530

    Add: create logic for getting form field value when submit

--- REACT-FORM/src/components/Login.jsx
+++ REACT-FORM/src/components/Login.jsx
@@ -1,23 +1,45 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [loginForm,setLoginForm]=useState({
+    email: '',
+    password: ''
+  });
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(loginForm);
+  }
+
+  function handleChange(event) {
+    setLoginForm((prevState)=>{
+      return {
+        ...prevState,
+        [event.target.name]: [event.target.value]
+      }
+    })
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={handleChange} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={handleChange} />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="reset" className="button button-flat">Reset</button>
+        <button type="submit" className="button">Login</button>
       </p>
     </form>
   );
