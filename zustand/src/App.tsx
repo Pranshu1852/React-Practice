2301649e44265f992cb573c5a6976b39a585f2f6
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-25 18:16:30 +0530

    Add: create small counter component that increment and decrement count

--- zustand/src/App.tsx
+++ zustand/src/App.tsx
@@ -1,7 +1,16 @@
+import { useCounterStore } from "./store";
+
 function App() {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increaseCount);
+  const decrement = useCounterStore((state) => state.decreaseCount);
+
   return (
     <div>
       <h2>Zustand</h2>
+      <h2>{count}</h2>
+      <button onClick={() => increment()}>Increment</button>
+      <button onClick={() => decrement()}>Decrement</button>
     </div>
   );
 }
--- zustand/src/store.ts
+++ zustand/src/store.ts
@@ -1,15 +1,13 @@
 import { create } from "zustand";
 
-interface StoreType {
+export interface StoreType {
   count: number;
   increaseCount: () => void;
   decreaseCount: () => void;
 }
 
-const useStore = create<StoreType>((set) => ({
+export const useCounterStore = create<StoreType>((set) => ({
   count: 0,
   increaseCount: () => set((state) => ({ count: state.count + 1 })),
   decreaseCount: () => set((state) => ({ count: state.count - 1 })),
 }));
-
-export default useStore;
